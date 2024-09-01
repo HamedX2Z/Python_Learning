d56db53cad33c15d81f5f41aae5912a7ec9b8041
Hamed <mortezarahmati1383@gmail.com>
2024-09-01 23:56:48 +0330

    Add main function to run the calculator with menu options
    A main function has been introduced to allow users to select and execute different calculator operations via a menu interface. This includes handling invalid inputs and providing feedback for each selected operation.

--- calculator.py
+++ calculator.py
@@ -23,3 +23,37 @@ def division():
     num1 = float(input("Please Enter the first Number: "))
     num2 = float(input("Please Enter the second Number: "))
     print(num1 / num2)
+
+# main function to run the calculator
+def main():
+    while True:
+        print("Select the operation that you want to perform\n")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. multiplication")
+        print("4. division\n")
+
+        try:
+            user_choice = int(input("Please Enter you're choice: "))
+
+            # Check if the choice is within the valid range
+            if user_choice == 1:
+                print("Great! You have selected Addition\n")
+                addition()
+            elif user_choice == 2:
+                print("Great! You have selected Subtraction\n")
+                subtraction()
+            elif user_choice == 3:
+                print("Great You have selected multiplication\n")
+                multiplication()
+            elif user_choice == 4:
+                print("Great You have selected division\n")
+                division()
+            else:
+                print("Invalid Choice! Please Enter a Number from 1-4\n")
+        except ValueError:
+            # Handle non-integer input
+            print("Invalid input! Please enter a whole number from 1-4.\n")
+
+# Runs the main function
+main()
