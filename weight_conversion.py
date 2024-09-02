e876daeeb4da90343721f88ed615d3585cee10db
Hamed <mortezarahmati1383@gmail.com>
2024-09-02 11:12:03 +0330

    Added a function to convert pounds to kilograms
    Implemented a new function named 'pound_to_kilo' in 'weight_conversion.py'. This function enables users to convert weights from pounds to kilograms by dividing the entered weight by 2.205 and prints out the result.

--- weight_conversion.py
+++ weight_conversion.py
@@ -6,3 +6,10 @@ def kilo_to_pound():
     pound = 2.205
     result = num1 * pound
     print(result)
+
+# pound to kilogram function
+def pound_to_kilo():
+    num1 = float(input("Please Enter the Second Number: "))
+    kilo = 2.205
+    result = num1 / kilo
+    print(result)
