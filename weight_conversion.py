e9e907a88ae2506e209fa7e774ab6553eba33d9a
Hamed <mortezarahmati1383@gmail.com>
2024-09-02 11:28:55 +0330

    Improve input validation and error handling
    Updated kilo_to_pound and pound_to_kilo functions to include try-except blocks for better handling of invalid input. Enhanced user prompts and output messages for improved clarity and user experience.

--- weight_conversion.py
+++ weight_conversion.py
@@ -2,14 +2,26 @@ print("Welcome to my Weight Conversion Program!")
 
 # kilogram to pound function
 def kilo_to_pound():
-    num1 = float(input("Please Enter the First Number: "))
-    pound = 2.205
-    result = num1 * pound
-    print(result)
+    try:
+        # Prompt the user for input and attempt to convert it to a float
+        user_input = float(input("Please Enter the Weight in Kilograms: "))
+        pound = 2.205
+        result = user_input * pound
+        print(f"{user_input} kilograms is equal to {result:.2f} pounds.")
+
+    except ValueError:
+        # If input is not a valid number, print an error message
+        print("Error: Please enter a valid number for the weight in kilograms.")
 
 # pound to kilogram function
 def pound_to_kilo():
-    num1 = float(input("Please Enter the Second Number: "))
-    kilo = 2.205
-    result = num1 / kilo
-    print(result)
+    try:
+        # prompt the user for input and then converts it into float
+        user_input = float(input("Please Enter the Second Number: "))
+        kilo = 2.205
+        result = user_input / kilo
+        print(f"{user_input} kilograms is equal to {result:.2f} pounds.")
+
+    except ValueError:
+        # If input is not a valid number, print an error message
+        print("Error: Please enter a valid number for the weight in pounds.")
