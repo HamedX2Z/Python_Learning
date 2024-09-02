ced63b6de2ccbd1fda52a74c8c2f9625df064563
Hamed <mortezarahmati1383@gmail.com>
2024-09-02 13:38:31 +0330

    Add user interaction for registration flow
    Display a welcome message and prompt for username and password. Validate password strength continuously until a suitable password is provided.
    
    Fixed typo and removed an unused variable

--- user_registration.py
+++ user_registration.py
@@ -1,10 +1,11 @@
+print("Welcome to the Sign up page!\n")
+
 def is_strong_password():
 
     # Initialize criteria flags
     has_upper = False
     has_lower = False
     has_digit = False
-    has_number = False
     has_special = False
     special_characters = "!@#$%^&*()-+"
 
@@ -15,7 +16,7 @@ def is_strong_password():
 
     # Loop through each character in the password to check criteria
     for char in password:
-        if char.isdigital():
+        if char.isdigit():
             has_digit = True
         elif char.isupper():
             has_upper = True
@@ -35,3 +36,15 @@ def is_strong_password():
 
     #return True only if all criteria are met
     return has_upper and has_lower and has_digit and has_special
+
+# Main part of the program: get username and password from the user
+username = input("Please Enter a username: ")
+
+# Loop until a strong password is provided
+while True:
+    password = input("Please Enter a password: ")
+    if is_strong_password():
+        print("Registration Successful!")
+        break
+    else:
+        print("Please try again with a strong password.")
