f22f733ce6d386a0bcd629278ffdbbedcb9f1bea
Hamed <mortezarahmati1383@gmail.com>
2024-09-02 11:07:25 +0330

    Created the kilogram to pound function

---
+++ weight_conversion.py
@@ -0,0 +1,8 @@
+print("Welcome to my Weight Conversion Program!")
+
+# kilogram to pound function
+def kilo_to_pound():
+    num1 = float(input("Please Enter the First Number: "))
+    pound = 2.205
+    result = num1 * pound
+    print(result)
