ace5aafff2ac4a901ff1e7188e9e07d4c14bfd0d
Hamed <mortezarahmati1383@gmail.com>
2024-09-02 11:41:32 +0330

    Add main function to run weight conversion program
    Introduced a main function that offers a user choice menu for converting between kilograms and pounds or exiting the program. This update enhances user interaction by incorporating input validation and adding a new method to gracefully exit the program.

--- weight_conversion.py
+++ weight_conversion.py
@@ -1,3 +1,5 @@
+from struct import pack_into
+
 print("Welcome to my Weight Conversion Program!")
 
 # kilogram to pound function
@@ -7,7 +9,7 @@ def kilo_to_pound():
         user_input = float(input("Please Enter the Weight in Kilograms: "))
         pound = 2.205
         result = user_input * pound
-        print(f"{user_input} kilograms is equal to {result:.2f} pounds.")
+        print(f"{user_input} kilograms is equal to {result:.2f} pounds.\n")
 
     except ValueError:
         # If input is not a valid number, print an error message
@@ -20,8 +22,34 @@ def pound_to_kilo():
         user_input = float(input("Please Enter the Second Number: "))
         kilo = 2.205
         result = user_input / kilo
-        print(f"{user_input} kilograms is equal to {result:.2f} pounds.")
+        print(f"{user_input} kilograms is equal to {result:.2f} pounds.\n")
 
     except ValueError:
         # If input is not a valid number, print an error message
         print("Error: Please enter a valid number for the weight in pounds.")
+
+#function to exit the game
+def exit_game():
+    print("Exiting the program. Goodbye!")
+    exit()
+
+# main function to run the program
+def main():
+    while True:
+        print("Select the operation that you want to perform\n")
+        print("1. Kilogram to Pound")
+        print("2. Pound to Kilogram")
+        print("3. Exit\n")
+        try:
+            user_choice = int(input("Please Enter you're choice: "))
+            if user_choice == 1:
+                kilo_to_pound()
+            elif user_choice == 2:
+                pound_to_kilo()
+            elif user_choice == 3:
+                exit_game()
+            else:
+                print("Invalid Choice! Please Enter a Number from 1-2\n")
+        except ValueError:
+            print("Invalid input! Please enter a whole number from 1-2.\n")
+main()
