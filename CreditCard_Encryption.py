4bcc6d3a504f398b9e9dcefc201ec18298893e8e
Hamed <mortezarahmati1383@gmail.com>
2024-09-03 12:46:34 +0330

    Refactor credit card number validation logic
    Simplify the validation logic by removing spaces before checks and ensuring the input contains only digits. Additionally, correct the error messages and maintain the proper formatting while displaying the encrypted credit card number.

--- CreditCard_Encryption.py
+++ CreditCard_Encryption.py
@@ -4,11 +4,16 @@ while True:
     # Asks user for a Credit Number to Encrypt
     CreditCard_Number = input("\nEnter Your Credit Card Number: ")
 
-    # if statement to check if user input contains any invalid data
-    if CreditCard_Number.replace(" ", "").isalpha():
-        print("Credit Card Number cant contain any letters or spaces!")
-    elif len(CreditCard_Number) < 12 or len(CreditCard_Number) > 12:
-        print("Credit Card Number must be at 12 digits long!")
+    # Removes spaces and checks for invalid characters
+    sanitized_number = CreditCard_Number.replace(" ", "")
+
+    # if statement to check if the input contains only digits
+    if not sanitized_number.isdigit():
+        print("Credit Card Number Must only contain digits!")
+    # Check if the sanitized number is exactly 12 digits long
+    elif len(sanitized_number) != 12 :
+        print("Credit Card Number must be 12 digits long!")
     else:
-        print(f"Here's Your Encrypted Credit Card Number: XXXX-XXXX-XXXX-{CreditCard_Number[-4:]}")
+        # Format the output with only the last 4 digits visible
+        print(f"Here's Your Encrypted Credit Card Number: XXXX-XXXX-XXXX-{sanitized_number[-4:]}")
         break
