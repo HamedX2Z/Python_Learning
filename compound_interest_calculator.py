76c42ff9c05b2185de2296cbb17b2451ed02a7c5
Hamed <mortezarahmati1383@gmail.com>
2024-09-04 14:40:42 +0330

    Refactor input validation into a function
    Modularized the input validation for investment amount into a dedicated function. This improves code readability and makes the input logic reusable in other parts of the program.

--- compound_interest_calculator.py
+++ compound_interest_calculator.py
@@ -1,12 +1,16 @@
 print("Welcome to Compound Interest Calculator\n")
 
-while True:
-    try:
-        investment = float(input("Please Enter the Amount of Investment: "))
-        if investment <= 0:
-            print("Investment cant be euqal or less than zero!")
-        else:
-            print(f"Set! Investment Amount is {investment}$")
-            break
-    except ValueError:
-        print("Invalid input! Please Enter a Number!\n")
+# function to get the user investment amount
+def investment():
+    while True:
+        # try block to check for possible invalid inputs
+        try:
+            investment_amount = float(input("Please Enter the Amount of Investment: "))
+            if investment_amount <= 0:
+                print("Investment cant be euqal or less than zero!")
+            else:
+                print(f"Set! Investment Amount is {investment_amount}$")
+                # returning the investment amount value so it can be used in other parts of the code
+                return investment_amount
+        except ValueError:
+            print("Invalid input! Please Enter a Number!\n")
