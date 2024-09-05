64f8bcdbaa3068e05ca17ee316376d5f11117fe4
Hamed <mortezarahmati1383@gmail.com>
2024-09-05 23:17:45 +0330

    Simplify user quit logic
    Refactor conditional statement to directly compare lowercased input. This enhances code readability by eliminating an unnecessary variable.

--- shopping_cart.py
+++ shopping_cart.py
@@ -6,7 +6,6 @@ total = 0
 
 while True:
     food = input("Enter a food to buy (press Q to quit): ")
-    user_input = food.lower()
-    if user_input == "q":
+    if food.lower() == "q":
         print("\nGoodbye for now!")
         break
