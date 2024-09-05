2e20986ae9a27235fac762d0df90841c1e7ea1d3
Hamed <mortezarahmati1383@gmail.com>
2024-09-05 23:43:22 +0330

    Handle invalid input for food price in shopping_cart.py
    Ensure the program handles non-numeric input gracefully by adding input validation for price entry. This prevents the program from crashing and prompts the user to enter a valid number.

--- shopping_cart.py
+++ shopping_cart.py
@@ -10,9 +10,15 @@ while True:
         print("\nGoodbye for now!")
         break
     else:
-        price = float(input(f"Enter the price of a {food}: $"))
-        foods.append(food)
-        prices.append(price)
+        while True:
+            try:
+                price = float(input(f"Enter the price of a {food}: $"))
+                foods.append(food)
+                prices.append(price)
+            except ValueError:
+                print("Invalid Input! Please enter a valid number.")
+            else:
+                break
 
 print("\n-----YOUR CART-----")
 for food in foods:
