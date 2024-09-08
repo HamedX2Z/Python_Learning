4e8a126c5cd902b840039d1e135869fe47e5d58b
Hamed <mortezarahmati1383@gmail.com>
2024-09-08 23:17:39 +0330

    added elif statements to check if the user input is None of not

--- Resturant_menu.py
+++ Resturant_menu.py
@@ -14,3 +14,13 @@ for key, value in menu.items():
     print(f"{key:10}: ${value:.2f}")
 
 print("------------------------")
+
+while True:
+    food = input("What food would you like to buy? (Q to quit): ").lower()
+    if food == "q":
+        print("Goodbye for now!")
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+    elif menu.get(food) is None:
+        print("Please select an item from the menu!")
