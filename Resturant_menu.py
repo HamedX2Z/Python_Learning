17e26bbd21a8c6222d3d830980bea77db2a8dcb4
Hamed <mortezarahmati1383@gmail.com>
2024-09-08 23:53:57 +0330

    wrote comment for sections of the code

--- Resturant_menu.py
+++ Resturant_menu.py
@@ -1,3 +1,4 @@
+# created a menu 2D list
 menu = {"pizza": 3.00,
         "nachos":4.50,
         "popcorn":6.00,
@@ -15,6 +16,7 @@ for key, value in menu.items():
 
 print("------------------------")
 
+#while loop to ask the user for input and check for invalid input
 while True:
     food = input("What food would you like to buy? (Q to quit): ").lower()
     if food == "q":
@@ -25,6 +27,7 @@ while True:
     elif menu.get(food) is None:
         print("Please select an item from the menu!")
 
+#for loop to print the cart
 for food in cart:
     total += menu.get(food)
     print(food, end=" ")
